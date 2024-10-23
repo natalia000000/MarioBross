5a4d28d85e8c442b89a989ccc9f3a7b2249e435e
natalia000000 <152176051+natalia000000@users.noreply.github.com>
2024-10-23 15:02:08 -0300

    Add files via upload

--- SuperPoder.java
+++ SuperPoder.java
@@ -1,11 +1,12 @@
 public interface SuperPoder {
 
     public int comerEstrella();
-        System.out.println("Tienes 10 segundos");
 
+   
     public boolean comerHongo();
 
-    while true;
+
+    
 
 
 } 
